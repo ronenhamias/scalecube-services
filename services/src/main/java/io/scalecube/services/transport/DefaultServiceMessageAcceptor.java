d9c0f1de75e578d3d4398a5f5d4c5e88e2882d04
segabriel <sergiy.gabriel@jivygroup.com>
2018-05-15 15:39:12 +0300

    Refactoring acceptor

--- services/src/main/java/io/scalecube/services/transport/DefaultServiceMessageAcceptor.java
+++ services/src/main/java/io/scalecube/services/transport/DefaultServiceMessageAcceptor.java
@@ -5,10 +5,7 @@ import io.scalecube.services.api.ServiceMessageHandler;
 
 import org.reactivestreams.Publisher;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.UnicastProcessor;
 
 public final class DefaultServiceMessageAcceptor implements ServiceMessageHandler {
 
@@ -20,40 +17,10 @@ public final class DefaultServiceMessageAcceptor implements ServiceMessageHandle
 
   @Override
   public Publisher<ServiceMessage> invoke(Publisher<ServiceMessage> publisher) {
-    return Flux.from(toHeadAndTail(publisher)).flatMap(pair -> {
-      ServiceMessage message = pair.head;
+    Flux<ServiceMessage> messages = Flux.from(publisher).share();
+    return messages.take(1).flatMap(message -> {
       ServiceMessageHandler dispatcher = serviceHandlers.get(message.qualifier());
-      return dispatcher.invoke(Flux.from(pair.tail).startWith(message));
+      return dispatcher.invoke(messages.startWith(message));
     });
-  }
-
-  private Publisher<HeadAndTail> toHeadAndTail(Publisher<ServiceMessage> publisher) {
-    AtomicBoolean first = new AtomicBoolean(true);
-    UnicastProcessor<ServiceMessage> tail = UnicastProcessor.create();
-    UnicastProcessor<HeadAndTail> firstResult = UnicastProcessor.create();
-
-    return Flux.from(publisher)
-        .doOnComplete(tail::onComplete)
-        .doOnError(tail::onError)
-        .flatMap(message -> {
-          if (first.compareAndSet(true, false)) {
-            firstResult.onNext(new HeadAndTail(message, tail));
-            firstResult.onComplete();
-            return firstResult;
-          } else {
-            tail.onNext(message);
-            return Flux.empty();
-          }
-        });
-  }
-
-  private static class HeadAndTail {
-    private final ServiceMessage head;
-    private final Publisher<ServiceMessage> tail;
-
-    public HeadAndTail(ServiceMessage head, Publisher<ServiceMessage> tail) {
-      this.head = head;
-      this.tail = tail;
-    }
   }
 }
