8943f3ffa08a3c2e4d647513f1093ce4fc38f31b
Ronen Nachmias <ronen.hamias@gmail.com>
2018-06-08 21:43:54 +0300

    improve request many benchmark runner to count number of batch completed (size 10K each) and how many events returned each
    add parallelizem and run on operator to improve throughput

--- benchmarks/src/main/java/io/scalecube/services/benchmarks/RequestManyBenchmarksRunner.java
+++ benchmarks/src/main/java/io/scalecube/services/benchmarks/RequestManyBenchmarksRunner.java
@@ -1,5 +1,6 @@
 package io.scalecube.services.benchmarks;
 
+import com.codahale.metrics.Meter;
 import com.codahale.metrics.Timer;
 
 import java.util.stream.LongStream;
@@ -9,19 +10,24 @@ import reactor.core.publisher.Flux;
 public class RequestManyBenchmarksRunner {
 
   public static void main(String[] args) {
-    ServicesBenchmarksSettings settings = ServicesBenchmarksSettings.from(args).build();
+    ServicesBenchmarksSettings settings = ServicesBenchmarksSettings.from(args)
+        .responseCount(10000)
+        .build();
     ServicesBenchmarksState state = new ServicesBenchmarksState(settings, new BenchmarkServiceImpl());
     state.setup();
 
     BenchmarkService benchmarkService = state.service(BenchmarkService.class);
     int responseCount = settings.responseCount();
     Timer timer = state.timer();
-
+    Meter meter = state.registry().meter("requestMany/responses");
     Flux.merge(Flux.fromStream(LongStream.range(0, Long.MAX_VALUE).boxed())
-        .publishOn(state.scheduler())
+        .parallel(Runtime.getRuntime().availableProcessors())
+        .runOn(state.scheduler())
         .map(i -> {
           Timer.Context timeContext = timer.time();
-          return benchmarkService.requestMany(responseCount).doOnNext(next -> timeContext.stop());
+          return benchmarkService.requestMany(responseCount)
+              .doOnNext(onNext->meter.mark())
+              .doFinally(next -> timeContext.stop());
         }))
         .take(settings.executionTaskTime())
         .blockLast();
