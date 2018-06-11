3f35eae6ca5ad285be32715858046ecdbefa00a5
artem-v <artem.vysochyn@jivygroup.com>
2018-06-11 14:06:04 +0300

    Refactoring at RSocketClientTransport.java to be able to get error signals on connectivity problems

--- rsocket-transport/src/main/java/io/scalecube/services/transport/rsocket/client/RSocketClientTransport.java
+++ rsocket-transport/src/main/java/io/scalecube/services/transport/rsocket/client/RSocketClientTransport.java
@@ -9,8 +9,10 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.rsocket.RSocket;
 import io.rsocket.RSocketFactory;
+import io.rsocket.exceptions.ConnectionErrorException;
 import io.rsocket.transport.netty.client.TcpClientTransport;
 
+import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,15 +21,14 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.MonoProcessor;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
 import reactor.ipc.netty.tcp.TcpClient;
 
 public class RSocketClientTransport implements ClientTransport {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(RSocketClientTransport.class);
 
-  private final Map<Address, Mono<RSocket>> rSockets = new ConcurrentHashMap<>();
+  private final ThreadLocal<Map<Address, Publisher<RSocket>>> rSockets =
+      ThreadLocal.withInitial(ConcurrentHashMap::new);
 
   private final ServiceMessageCodec codec;
 
@@ -37,17 +38,32 @@ public class RSocketClientTransport implements ClientTransport {
 
   @Override
   public ClientChannel create(Address address) {
-    final Map<Address, Mono<RSocket>> monoMap = rSockets; // keep reference for threadsafety
-    Mono<RSocket> rSocket = monoMap.computeIfAbsent(address, address1 -> connect(address1, monoMap));
+    final Map<Address, Publisher<RSocket>> monoMap = rSockets.get(); // keep reference for threadsafety
+    Publisher<RSocket> rSocket = monoMap.computeIfAbsent(address, address1 -> connect(address1, monoMap));
     return new RSocketServiceClientAdapter(rSocket, codec);
   }
 
-  private static Mono<RSocket> connect(Address address, Map<Address, Mono<RSocket>> monoMap) {
-    MonoProcessor<Scheduler> schedulerProcessor = MonoProcessor.create();
+  private static Mono<RSocket> connect(Address address, Map<Address, Publisher<RSocket>> monoMap) {
     MonoProcessor<RSocket> rSocketProcessor = MonoProcessor.create();
 
     RSocketFactory.connect()
-        .transport(createTcpClientTransport(monoMap, address, schedulerProcessor))
+        .transport(TcpClientTransport.create(
+            TcpClient.create(options -> options
+                .disablePool()
+                .host(address.host())
+                .port(address.port())
+                .afterNettyContextInit(nettyContext -> {
+                  // add handler to react on remote node closes connection
+                  nettyContext.addHandler(new ChannelInboundHandlerAdapter() {
+                    @Override
+                    public void channelInactive(ChannelHandlerContext ctx) {
+                      monoMap.remove(address);
+                      LOGGER.debug("Connection became inactive on {} and removed from monoMap", address);
+                      rSocketProcessor.onError(new ConnectionErrorException("channelInactive"));
+                      ctx.fireChannelInactive();
+                    }
+                  });
+                }))))
         .start()
         .subscribe(
             rSocket -> {
@@ -55,9 +71,9 @@ public class RSocketClientTransport implements ClientTransport {
               rSocket.onClose().subscribe(aVoid -> {
                 monoMap.remove(address);
                 LOGGER.debug("Connection closed on {} and removed from the pool", address);
+                rSocketProcessor.onError(new ConnectionErrorException("onClose"));
               });
               rSocketProcessor.onNext(rSocket);
-              rSocketProcessor.onComplete();
             },
             throwable -> {
               LOGGER.warn("Connect failed on {}, cause: {}", address, throwable);
@@ -65,32 +81,7 @@ public class RSocketClientTransport implements ClientTransport {
               rSocketProcessor.onError(throwable);
             });
 
-    return schedulerProcessor.flatMap(rSocketProcessor::subscribeOn);
+    return rSocketProcessor;
   }
 
-  private static TcpClientTransport createTcpClientTransport(Map<Address, Mono<RSocket>> monoMap,
-      Address address, MonoProcessor<Scheduler> schedulerProcessor) {
-
-    return TcpClientTransport.create(
-        TcpClient.create(options -> options
-            .disablePool()
-            .host(address.host())
-            .port(address.port())
-            .afterNettyContextInit(nettyContext -> {
-              // add handler to react on remote node closes connection
-              nettyContext.addHandler(new ChannelInboundHandlerAdapter() {
-                @Override
-                public void channelInactive(ChannelHandlerContext ctx) {
-                  monoMap.remove(address);
-                  LOGGER.debug("Connection became inactive on {} and removed from monoMap", address);
-                  ctx.fireChannelInactive();
-                }
-              });
-              // expose executor where channel was assigned
-              Scheduler scheduler = Schedulers.fromExecutor(nettyContext.channel().eventLoop());
-              schedulerProcessor.onNext(scheduler);
-              schedulerProcessor.onComplete();
-              LOGGER.debug("Obtained scheduler {} on channel {}", scheduler, nettyContext.channel());
-            })));
-  }
 }
