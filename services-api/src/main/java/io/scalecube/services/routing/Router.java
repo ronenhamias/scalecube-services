9b82d77bce6dd5c08862aaa97b94595d54a95118
Dmytro Lazebnyi <dmytro.lazebnyi@jivygroup.com>
2018-06-19 18:26:56 +0300

    Remove unnecessary method `routes` from Router interface

--- services-api/src/main/java/io/scalecube/services/routing/Router.java
+++ services-api/src/main/java/io/scalecube/services/routing/Router.java
@@ -4,7 +4,6 @@ import io.scalecube.services.ServiceReference;
 import io.scalecube.services.api.ServiceMessage;
 import io.scalecube.services.registry.api.ServiceRegistry;
 
-import java.util.List;
 import java.util.Optional;
 
 @FunctionalInterface
@@ -18,16 +17,5 @@ public interface Router {
    * @return service instance
    */
   Optional<ServiceReference> route(ServiceRegistry serviceRegistry, ServiceMessage request);
-
-  /**
-   * Returns all suitable service references for particular service message.
-   *
-   * @param serviceRegistry service registry
-   * @param request service message
-   * @return list of suitable routes
-   */
-  default List<ServiceReference> routes(ServiceRegistry serviceRegistry, ServiceMessage request) {
-    return serviceRegistry.lookupService(request.qualifier());
-  }
 
 }
--- services/src/main/java/io/scalecube/services/routing/RandomServiceRouter.java
+++ services/src/main/java/io/scalecube/services/routing/RandomServiceRouter.java
@@ -12,7 +12,7 @@ public class RandomServiceRouter implements Router {
 
   @Override
   public Optional<ServiceReference> route(ServiceRegistry serviceRegistry, ServiceMessage request) {
-    List<ServiceReference> serviceInstances = routes(serviceRegistry, request);
+    List<ServiceReference> serviceInstances = serviceRegistry.lookupService(request.qualifier());
     if (!serviceInstances.isEmpty()) {
       int index = ThreadLocalRandom.current().nextInt((serviceInstances.size()));
       return Optional.of(serviceInstances.get(index));
