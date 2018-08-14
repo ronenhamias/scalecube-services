f89790a31050abccf2eb2e7c2f33cc21b2095312
Ronen Nachmias <ronen.hamias@gmail.com>
2018-08-14 10:46:10 +0300

    update branch

--- aeron-services-transport/src/main/java/io/scalecube/services/transport/aeron/AeronServiceTransport.java
+++ aeron-services-transport/src/main/java/io/scalecube/services/transport/aeron/AeronServiceTransport.java
@@ -1,5 +1,6 @@
 package io.scalecube.services.transport.aeron;
 
+import reactor.core.publisher.Mono;
 import io.scalecube.services.codec.HeadersCodec;
 import io.scalecube.services.codec.ServiceMessageCodec;
 import io.scalecube.services.transport.ServiceTransport;
@@ -7,6 +8,7 @@ import io.scalecube.services.transport.aeront.client.AeronClientTransport;
 import io.scalecube.services.transport.aeront.server.AeronServerTransport;
 import io.scalecube.services.transport.client.api.ClientTransport;
 import io.scalecube.services.transport.server.api.ServerTransport;
+import java.util.concurrent.ExecutorService;
 
 public class AeronServiceTransport implements ServiceTransport {
 
@@ -22,5 +24,17 @@ public class AeronServiceTransport implements ServiceTransport {
   public ServerTransport getServerTransport() {
     HeadersCodec headersCodec = HeadersCodec.getInstance(DEFAULT_HEADERS_FORMAT);
     return new AeronServerTransport(new ServiceMessageCodec(headersCodec));
+  }
+
+  @Override
+  public ExecutorService getExecutorService() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Mono<Void> shutdown() {
+    // TODO Auto-generated method stub
+    return null;
   }
 }
